2a92f8cb6546f9ecf5df7ad5836ca4afa6e5aed0
RogerSteven1104 <rogersteven2013@gmail.com>
2026-08-07 18:45:08 -0500

    Implementar estructura inicial de Usuario

---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	usuario := NuevoUsuario(
+		1,
+		"Roger",
+		"roger@email.com",
+		"123456",
+		"Premium",
+	)
+
+	usuario.MostrarInformacion()
+}
---
+++ src/usuario.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Usuario representa a una persona registrada
+// dentro del Sistema de Gestión de Streaming.
+type Usuario struct {
+	idUsuario       int
+	nombre          string
+	correo          string
+	contrasena      string
+	planSuscripcion string
+}
+
+// NuevoUsuario crea y devuelve un nuevo usuario
+// con los datos recibidos como parámetros.
+func NuevoUsuario(id int, nombre string, correo string, contrasena string, plan string) *Usuario {
+	return &Usuario{
+		idUsuario:       id,
+		nombre:          nombre,
+		correo:          correo,
+		contrasena:      contrasena,
+		planSuscripcion: plan,
+	}
+}
+
+// MostrarInformacion muestra los datos básicos del usuario.
+func (u Usuario) MostrarInformacion() {
+	fmt.Println("ID:", u.idUsuario)
+	fmt.Println("Nombre:", u.nombre)
+	fmt.Println("Correo:", u.correo)
+	fmt.Println("Plan:", u.planSuscripcion)
+}
